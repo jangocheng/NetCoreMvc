d7ae2f772eb1fd54f4ce151f1294459048a3b4c2
MemorySum <1433028369@qq.com>
2018-05-30 17:19:09 +0800

    修改

--- NetCoreMvc.Web/Startup.cs
+++ NetCoreMvc.Web/Startup.cs
@@ -43,7 +43,7 @@ namespace NetCoreMvc
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");   
             });
         }
     }
